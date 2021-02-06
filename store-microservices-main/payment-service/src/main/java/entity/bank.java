5f954255105d6188115b56d410de3111de26e31c
Rodrigo Amezcua Caballero <rodrigoamezcua@MacBook-Pro-de-Rodrigo.local>
2021-02-05 18:48:56 -0600

    Configuración y paquetes de payment-service

---
+++ store-microservices-main/payment-service/src/main/java/entity/bank.java
@@ -0,0 +1,17 @@
+package entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class bank {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+}
